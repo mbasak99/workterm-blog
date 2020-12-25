96f3af277af6621fefcc8a5c7d58421b8f6e9fc9
Monark Basak <monarkb01@gmail.com>
2020-12-25 18:36:26 -0500

    Made progress for kenna work term page.

--- pages/workterms/kenna.js
+++ pages/workterms/kenna.js
@@ -1,4 +1,4 @@
-import Head from "next/head";
+import Head from 'next/head';
 
 export default function KennaTerm() {
   return (
@@ -15,12 +15,30 @@ export default function KennaTerm() {
       </div>
       <div>
         <h2>My Goals</h2>
+        <ul>
+          <li>Become more experienced with JavaScript</li>
+          <li>Gain experience in a professional environment</li>
+          <li>
+            Gain experience using backend technologies such as NodeJS and SQL
+          </li>
+          <li>Become more experienced with CSS</li>
+          <li>Become more comfortable reading documentation</li>
+          <li>Become more adept at debugging code</li>
+        </ul>
       </div>
       <div>
         <h2>What I Learned</h2>
       </div>
       <div>
         <h2>Acknowledgements</h2>
+        <p>
+          A huge shoutout to Kenna, first of all, when I received the co-op
+          offer (and promptly accepted) COVID-19 troubles were right around the
+          corner but I was assured that I would still be able intern and they
+          would provide all their interns with equipment to safely work from
+          home. I was immensely grateful as I know not everyone in the co-op
+          program could say the same.
+        </p>
       </div>
     </>
   );
