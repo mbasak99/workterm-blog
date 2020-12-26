c3c8f3cb2348955668fe7b9e9901a37bfd76e010
Monark Basak <monarkb01@gmail.com>
2020-12-26 02:34:34 -0500

    Made significant progress for kenna workterm page.

--- pages/workterms/kenna.js
+++ pages/workterms/kenna.js
@@ -1,4 +1,4 @@
-import Head from 'next/head';
+import Head from "next/head";
 
 export default function KennaTerm() {
   return (
@@ -8,7 +8,7 @@ export default function KennaTerm() {
       </Head>
       <h1>Kenna (Summer 2020 - Fall 2020)</h1>
       <div>
-        <h2>First experience</h2>
+        <h2>First Experience</h2>
       </div>
       <div>
         <h2>About Kenna</h2>
@@ -27,17 +27,30 @@ export default function KennaTerm() {
         </ul>
       </div>
       <div>
+        <h2>Some Projects I Worked On</h2>
+        <ul>
+          <li>WALLTITE</li>
+          <li>BASF US/CAN</li>
+          <li>Retail Connect Rep Tool</li>
+          <li>BASF Event Management</li>
+        </ul>
+      </div>
+      <div>
         <h2>What I Learned</h2>
       </div>
       <div>
         <h2>Acknowledgements</h2>
         <p>
           A huge shoutout to Kenna, first of all, when I received the co-op
-          offer (and promptly accepted) COVID-19 troubles were right around the
-          corner but I was assured that I would still be able intern and they
-          would provide all their interns with equipment to safely work from
-          home. I was immensely grateful as I know not everyone in the co-op
-          program could say the same.
+          offer (which I promptly accepted) COVID-19 troubles were right around
+          the corner but I was assured that all incoming and existing co-op
+          students would still be able intern and they would provide all their
+          interns with all the necessary equipment to safely work from home. I
+          was immensely grateful as I know not everyone in the co-op program
+          could say the same. I also want to shoutout the great people
+          comprising Kenna for making the 8-month term enjoyable by hosting as
+          there were many events that had all the employees get together online
+          and just have fun, discuss topics, and/or learn something.
         </p>
       </div>
     </>
