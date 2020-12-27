2c50d7e744161bc9079cd7d975e7a439d590525d
Monark Basak <monarkb01@gmail.com>
2020-12-27 15:26:29 -0500

    Added more content for Kenna page.

--- pages/workterms/kenna.js
+++ pages/workterms/kenna.js
@@ -19,7 +19,7 @@ export default function KennaTerm() {
           <li>Become more experienced with JavaScript</li>
           <li>Gain experience in a professional environment</li>
           <li>
-            Gain experience using backend technologies such as NodeJS and SQL
+            Gain experience using backend technologies such as Node.js and SQL
           </li>
           <li>Become more experienced with CSS</li>
           <li>Become more comfortable reading documentation</li>
@@ -29,10 +29,12 @@ export default function KennaTerm() {
       <div>
         <h2>Some Projects I Worked On</h2>
         <ul>
-          <li>WALLTITE</li>
-          <li>BASF US/CAN</li>
-          <li>Retail Connect Rep Tool</li>
-          <li>BASF Event Management</li>
+          <li>WALLTITE (Handlebars, jQuery, CSS, Node.js, T-SQL)</li>
+          <li>BASF US/CAN (jQuery, CSS) In-Season Logistics</li>
+          <li>Retail Connect Rep Tool (jQuery, CSS)</li>
+          <li>BASF Event Management (React, CSS)</li>
+          <li>Market Connect (React, Redux, Node.js, T-SQL)</li>
+          <li>In-house tools (SQL, LotusScript)</li>
         </ul>
       </div>
       <div>
@@ -44,13 +46,23 @@ export default function KennaTerm() {
           A huge shoutout to Kenna, first of all, when I received the co-op
           offer (which I promptly accepted) COVID-19 troubles were right around
           the corner but I was assured that all incoming and existing co-op
-          students would still be able intern and they would provide all their
-          interns with all the necessary equipment to safely work from home. I
-          was immensely grateful as I know not everyone in the co-op program
-          could say the same. I also want to shoutout the great people
-          comprising Kenna for making the 8-month term enjoyable by hosting as
-          there were many events that had all the employees get together online
-          and just have fun, discuss topics, and/or learn something.
+          students would still be able intern and they would provide all of
+          their interns with all the necessary equipment to safely work from
+          home. I was immensely grateful as I know not everyone in the co-op
+          program could say the same. I also want to shoutout the great people
+          comprising Kenna for making the 8-month term enjoyable by hosting many
+          social events and activites as there were many events that had all the
+          employees from all the departments get together online and just have
+          fun, discuss topics, and/or learn something. I also would like to
+          shoutout the amazing developers I was fortunate enough to work with,
+          there was always someone ready to lend a hand whether it was to point
+          me to the right place for something, helping debugging a piece of
+          code, or anything else really. And last, but definitely not least, I
+          want to give a huge shoutout to my mentor and supervisor Nicholas
+          Paul, who took me under his wing and taught me so much about the tools
+          and technolgies I'd have to work with, for being patient and
+          understanding when I was stuck, and always being able to find the time
+          to help me out.
         </p>
       </div>
     </>
