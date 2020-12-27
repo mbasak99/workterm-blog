3d87a1a8422ee2b29b04a425598bbc25202da5e1
Monark Basak <monarkb01@gmail.com>
2020-12-26 19:10:44 -0500

    Finished resume page, just need to update my resume once this site is done.

--- pages/resume.js
+++ pages/resume.js
@@ -1,4 +1,11 @@
 import Head from "next/head";
+import dynamic from "next/dynamic";
+import { Button } from "antd";
+import styles from "../styles/resume.module.css";
+
+const ClientPDFViewer = dynamic(() => import("./components/pdf_viewer"), {
+  ssr: false,
+});
 
 export default function Resume() {
   return (
@@ -7,7 +14,17 @@ export default function Resume() {
         <title>Resume</title>
       </Head>
       <h1>Resume</h1>
-      {/* Place resume here */}
+      <div className={styles.resume_container}>
+        <ClientPDFViewer url="/Monark_Basak_Resume.pdf" height={1080} />
+        <Button
+          type="primary"
+          href="/Monark_Basak_Resume.pdf"
+          target="_blank"
+          shape="round"
+        >
+          View/Download as PDF
+        </Button>
+      </div>
     </>
   );
 }
