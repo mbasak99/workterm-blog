4fef93494569dd11d855aa0bd9ea1b6578c66fef
Monark Basak <monarkb01@gmail.com>
2020-12-31 20:49:43 -0500

    Removed max width limit for site and added more content for Kenna page.

--- pages/workterms/kenna.js
+++ pages/workterms/kenna.js
@@ -6,17 +6,32 @@ export default function KennaTerm() {
       <Head>
         <title>Kenna</title>
       </Head>
-      <h1>Kenna (Summer 2020 - Fall 2020)</h1>
+      <h1>Kenna (May 2020 - December 2020)</h1>
       <div>
         <h2>First Experience</h2>
+        <p>
+          I was a bit nervous when I first started out since it was my first
+          ever co-op work term, and I had never coded in a professional
+          environment before. I was also worried whether I'd be able to pick
+          things up that are specifc to Kenna such as utilizing certain tools in
+          a specific way, or things of that nature. But looking back now I think
+          that was foolish as I had great support from fellow developers and my
+          mentor who were always willing to lend a hand when I was stuck. Kenna
+          also documented their tools, projects and practically everything else
+          very well, if there's a question to be asked, it's probably been
+          answered in their Confluence documentation, and if it's not, then
+          you're encouraged to add to the documentation to help those in the
+          future.
+        </p>
       </div>
       <div>
         <h2>About Kenna</h2>
+        <p>Kenna provides Customer Relationship Management </p>
       </div>
       <div>
         <h2>My Goals</h2>
         <ul>
-          <li>Become more experienced with JavaScript</li>
+          <li>Become more experienced with JavaScript and JS libraries</li>
           <li>Gain experience in a professional environment</li>
           <li>
             Gain experience using backend technologies such as Node.js and SQL
@@ -39,6 +54,12 @@ export default function KennaTerm() {
       </div>
       <div>
         <h2>What I Learned</h2>
+        <p>
+          Working across the many projects I had been placed on, I've learned so
+          much from when I first started out. One of the first things I learned
+          was certain projects had to support older and outdated browsers which
+          can sometimes be a huge pain, I'm looking at you IE11.
+        </p>
       </div>
       <div>
         <h2>Acknowledgements</h2>
@@ -46,23 +67,23 @@ export default function KennaTerm() {
           A huge shoutout to Kenna, first of all, when I received the co-op
           offer (which I promptly accepted) COVID-19 troubles were right around
           the corner but I was assured that all incoming and existing co-op
-          students would still be able intern and they would provide all of
-          their interns with all the necessary equipment to safely work from
-          home. I was immensely grateful as I know not everyone in the co-op
-          program could say the same. I also want to shoutout the great people
-          comprising Kenna for making the 8-month term enjoyable by hosting many
-          social events and activites as there were many events that had all the
-          employees from all the departments get together online and just have
-          fun, discuss topics, and/or learn something. I also would like to
-          shoutout the amazing developers I was fortunate enough to work with,
-          there was always someone ready to lend a hand whether it was to point
-          me to the right place for something, helping debugging a piece of
-          code, or anything else really. And last, but definitely not least, I
-          want to give a huge shoutout to my mentor and supervisor Nicholas
-          Paul, who took me under his wing and taught me so much about the tools
-          and technolgies I'd have to work with, for being patient and
-          understanding when I was stuck, and always being able to find the time
-          to help me out.
+          students would still be able to work during their term(s) and they
+          would provide everyone with all the necessary equipment to safely work
+          from home. I was immensely grateful as I know not everyone in the
+          co-op program could say the same. I also want to shoutout the great
+          people comprising Kenna for making the 8-month term enjoyable by
+          hosting many social events and activites as there were many events
+          that had all the employees from all the departments get together
+          online and just have fun, discuss topics, and/or learn something. I
+          also would like to shoutout the amazing developers I was fortunate
+          enough to work with, there was always someone ready to lend a hand
+          whether it was to point me to the right place for something, helping
+          debugging a piece of code, or anything else really. And last, but
+          definitely not least, I want to give a huge shoutout to my mentor and
+          supervisor Nicholas Paul, who took me under his wing and taught me so
+          much about the tools and technolgies I'd have to work with, for being
+          patient and understanding when I was stuck, and always being able to
+          find the time to help me out even when his schedule would get hectic.
         </p>
       </div>
     </>
