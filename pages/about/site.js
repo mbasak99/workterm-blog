5316ac260838dae490043325e554ba105433ba45
Monark Basak <monarkb01@gmail.com>
2021-01-10 21:06:41 -0500

    Minor changes.

--- pages/about/site.js
+++ pages/about/site.js
@@ -8,21 +8,21 @@ export default function AboutSite() {
       </Head>
       <h1>About This Site</h1>
       <div>
-        <h2>Technologies used</h2>
+        <h2>Technologies Used</h2>
         <ul>
           <li>React</li>
-          <li>Next.js</li>
-          <li>Ant Design</li>
-          <li>Vercel</li>
-          <li>Framer Motion</li>
+          <li>Next.js (React framework with server-side rendering)</li>
+          <li>Ant Design (UI)</li>
+          <li>Vercel (Hosting)</li>
+          <li>Framer Motion (Animations)</li>
         </ul>
       </div>
       <div>
-        <h2>Why was this site made?</h2>
+        <h2>Why Was This Site Made?</h2>
         <p>
-          This site was made to help reflect on the work experience we gain(ed)
-          over the course of our co-op program. But also as a way to track our
-          own progress when looking back. I also wanted to create my own site as
+          This site was made to help reflect on the work experience we gain over
+          the course of our co-op program. But also as a way to track our own
+          progress when looking back. I also wanted to create my own site as
           opposed to using WordPress or some other site builder because I wanted
           to experiment with some technologies I had heard about.
         </p>
