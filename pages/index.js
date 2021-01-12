e003c42ce3792eef4fdf856f8e781201449b5e09
Monark Basak <monarkb01@gmail.com>
2021-01-12 15:11:04 -0500

    Added more content for intro.

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,9 @@ export default function Home() {
         Hey, I'm Monark Basak. I'm 21 years old and currently attending the
         University of Guelph pursuing a Bachelor of Computing degree as a
         Software Engineering student. As of writing this, I've finished an 8
-        month term co-op and about to enter the 3rd year of my program.
+        month term co-op and about to enter the 3rd year of my program. I've
+        recently finished working at Kenna working as a Fullstack Developer and
+        really enjoyed working with technologies pertaining to the web.
       </p>
     </>
   );
